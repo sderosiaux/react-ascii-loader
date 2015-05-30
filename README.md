7d06481cd440238a8b22ad2e12c10ebf806a41d8
Stéphane D <derste@gmail.com>
2015-05-30 11:10:23 +0200

    es6

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Description
 A simple ASCII loader : \|/—
 
+# Example
+
+```javascript
+// default loader
+<Loader />
+// fast loader with default characters set
+<Loader speed={20} />
+// simulate a blink
+<Loader characters=". " />
+// why not
+<Loader characters="abcdefghijklmnopqrstuvwxyz" speed={100} />
+```
+
 # Reference
-Idea from https://gist.github.com/nkbt/6549487
+Inspired from https://gist.github.com/nkbt/6549487
--- src/loader.js
+++ src/loader.js
@@ -1,33 +1,45 @@
 import React from 'react';
 
-const LOADER_CHARACTERS = "\\|/—";
-
+const LOADER_CHARACTERS = '\\|/—';
+const LOADER_SPEED_MS = 100;
 const LOADER_STYLE = {
-  fontFamily: "Consolas, \"Liberation Mono\", Courier, monospace"
+  fontFamily: 'Consolas, \"Liberation Mono\", Courier, monospace'
 };
 
-export default class Toto {
-    constructor() {
+export default class Toto extends React.Component {
+
+    static propTypes = {
+        characters: React.PropTypes.string,
+        speed: React.PropTypes.number
+    }
+
+    static defaultProps = {
+        characters: LOADER_CHARACTERS,
+        speed: LOADER_SPEED_MS
+    }
+
+    constructor(props) {
+        super(props);
         this.state = { tick: 0 };
     }
 
     componentWillMount() {
-    	this.interval = setInterval(::this.nextStep(), 100);
+        this.interval = setInterval(::this.nextStep(), this.props.speed);
     }
-    
+
     componentWillUnmount() {
-    	clearInterval(this.interval);
+        clearInterval(this.interval);
     }
 
     nextStep() {
-    	this.setState({
-            tick: (this.state.tick + 1) % LOADER_CHARACTERS.length
+        this.setState({
+            tick: (this.state.tick + 1) % this.props.characters.length
         });
     }
 
     render() {
       return (
-        <span style={LOADER_STYLE}>{ loading[this.state.tick] }</span>
+        <span style={LOADER_STYLE}>{ this.props.characters[this.state.tick] }</span>
       );
     }
 
